a686bc5634921754af00f7f0985682234d99d1d6
Saradhi Indurthi <saradhix@yahoo.com>
2012-04-05 12:45:23 +0530

    Modified endian.c

--- endian.c
+++ endian.c
@@ -4,6 +4,6 @@ int x=0x12345678;
 char *p=&x;
 printf("%x %x\n",*p,x);
 }
-/*On bigendian machines the output is 78 12345678*/
-/*On little endian machines the output is 12 12345678*/
+/*On little endian machines the output is 78 12345678*/
+/*On big endian machines the output is 12 12345678*/
 //Guess why ?
